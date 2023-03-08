b09be4b6a1b04a0a8ebf53314b672912c2c59ee2
Alan Costa <peixesponja@gmail.com>
2023-03-08 18:53:34 -0300

    ajustes

--- main/rcmonitor.ino
+++ main/rcmonitor.ino
@@ -27,15 +27,11 @@
 
 
 
-#ifdef ESP32
-  static NimBLEServer* pServer;
-  static NimBLECharacteristic* monitorConfigCharacteristic;
-  static NimBLECharacteristic* monitorNotificationCharacteristic;
-#else
-  BLEService mainService = BLEService(MAIN_SERVICE_NAME);
-  BLECharacteristic monitorConfigCharacteristic = BLECharacteristic (CONFIG_CHARACTERISTIC);
-  BLECharacteristic monitorNotificationCharacteristic = BLECharacteristic (NOTIFICATION_CHARACTERISTIC);
-#endif 
+
+static NimBLEServer* pServer;
+static NimBLECharacteristic* monitorConfigCharacteristic;
+static NimBLECharacteristic* monitorNotificationCharacteristic;
+
 
 
 boolean wasConnected = true;
@@ -76,15 +72,11 @@ boolean sendConfigCommand(int cmdType, int monitorId, const char* payload, int p
     bytes[1] = (byte)monitorId;
     bytes[2] = (byte)payloadSequence;
     memcpy(bytes + 3, payloadPart, strlen(payloadPart));
-#ifdef ESP32
+
     monitorConfigCharacteristic->setValue(bytes, 3 + strlen(payloadPart));
     monitorConfigCharacteristic->indicate();
     delay(50); 
-#else
-    if (!monitorConfigCharacteristic.indicate(bytes,  3 + strlen(payloadPart))) {
-        return false;
-    }
-#endif
+    
     // Handle remaining payload
     if (remainingPayload) {
         boolean r = sendConfigCommand(CMD_TYPE_ADD, monitorId, remainingPayload, payloadSequence + 1);
@@ -121,7 +113,6 @@ boolean configureMonitors() {
 }
 
 
-#ifdef ESP32  // ---------------------------------------------------------------------------
 class monitorConfigWriteCallback: public NimBLECharacteristicCallbacks {
     void onWrite(NimBLECharacteristic* pCharacteristic) {
         if (pCharacteristic->getValue().length() >= 1) {
@@ -160,45 +151,6 @@ class monitorNotificationWriteCallback: public NimBLECharacteristicCallbacks {
     };
 };
         
-#else  // ---------------------------------------------------------------------------
-void monitorConfigWriteCallback(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uint16_t len) {
-        if (len >= 1) {
-            int result = data[0];
-            int monitorId = data[1];
-            switch (result) {
-                case CMD_RESULT_PAYLOAD_OUT_OF_SEQUENCE:
-                    Serial.println("");
-                    Serial.print(monitorId);
-                    Serial.println(" out-of-sequence");
-                    break;
-                case CMD_RESULT_EQUATION_EXCEPTION:
-                    // Notice exception details available in the data
-                    Serial.println("");
-                    Serial.print(monitorId);
-                    Serial.println(" exception");
-                    break;
-                default:
-                    break;
-            }
-        }
-}
-
-void monitorNotificationWriteCallback(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uint16_t len) {
-        int dataPos = 0;
-        while (dataPos + 5 <= len) {
-            int monitorId = (int)data[dataPos];
-            int32_t value = data[dataPos + 1] << 24 | data[dataPos + 2] << 16 | data[dataPos + 3] << 8 | data[dataPos + 4];
-            if (monitorId < nextMonitorId) {
-                monitorValues[monitorId] = value;
-            }
-            dataPos += 5;
-        }
-}
-#endif  // ---------------------------------------------------------------------------
-        
-
-
-
 
 void handleConnected() {
     // Reset state
@@ -213,14 +165,10 @@ void handleConnected() {
 #endif
 }
 
-
 void handleDisconnected() {
     Serial.println("No BLE connection");
 }
 
-
-
-#ifdef ESP32  // ---------------------------------------------------------------------------
 boolean handleConfigure() {
     // Start configuring when inidicate is enabled for the first time 
     if (!monitorConfigStarted) {
@@ -241,34 +189,7 @@ boolean handleConfigure() {
     }
     return true;
 }
-#else  // ---------------------------------------------------------------------------
-boolean handleConfigure() {
-    // Start configuring when inidicate is enabled for the first time 
-    if (!monitorConfigStarted) {
-        boolean isIndicating = monitorConfigCharacteristic.indicateEnabled();
-        if (isIndicating) {
-            // Configure in loop until success or disconnected
-            monitorConfigStarted = true;
-            while (1) {
-                Serial.print("Configuring... ");    
-                if (!Bluefruit.connected() || configureMonitors()) {
-                    Serial.println("Done");    
-                    return true;
-                }
-                Serial.println("Fail");    
-            }
-        }
-        return false;
-    }
-    return true;
-}
-#endif  // ---------------------------------------------------------------------------
 
-
-
-
-
-#ifdef ESP32  // ---------------------------------------------------------------------------
 static monitorConfigWriteCallback cbmonitorConfigWriteCallback;
 static monitorNotificationWriteCallback cbmonitorNotificationWriteCallback;
 
@@ -306,63 +227,15 @@ void bluetoothStart(void) {
     pAdvertising->start();
 }
 
-
-#else  // ---------------------------------------------------------------------------
-void monitorConfigWriteCallback(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uint16_t len);
-void monitorNotificationWriteCallback(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uint16_t len);
-
-void bluetoothSetupMainService() {
-    mainService.begin();
-    monitorConfigCharacteristic.setProperties(CHR_PROPS_INDICATE | CHR_PROPS_WRITE);
-    monitorConfigCharacteristic.setPermission(SECMODE_OPEN, SECMODE_OPEN);
-    monitorConfigCharacteristic.setWriteCallback(monitorConfigWriteCallback);
-    monitorConfigCharacteristic.begin();
-    monitorNotificationCharacteristic.setProperties(CHR_PROPS_WRITE_WO_RESP);
-    monitorNotificationCharacteristic.setPermission(SECMODE_NO_ACCESS, SECMODE_OPEN);
-    monitorNotificationCharacteristic.setWriteCallback(monitorNotificationWriteCallback);
-    monitorNotificationCharacteristic.begin();
-}
-
-void bluetoothStartAdvertising(void) {
-    Bluefruit.setTxPower(+4);
-    Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
-    Bluefruit.Advertising.addTxPower();
-    Bluefruit.Advertising.addService(mainService);
-    Bluefruit.Advertising.addName();
-    Bluefruit.Advertising.restartOnDisconnect(true);
-    Bluefruit.Advertising.setInterval(160, 160); // in unit of 0.625 ms
-    Bluefruit.Advertising.setFastTimeout(30);
-    Bluefruit.Advertising.start(0);
-}
-
-void bluetoothStart() {
-    Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
-    Bluefruit.begin();
-    uint8_t mac[6] = { 0, 0, 0, 0, 0, 0 };
-    Bluefruit.getAddr(mac);
-    char name[255];
-    snprintf(name, sizeof(name), "RC DIY #%02X%02X", mac[4], mac[5]);
-    Bluefruit.setName(name);     
-    bluetoothSetupMainService();
-    bluetoothStartAdvertising(); 
-}
-#endif  // ---------------------------------------------------------------------------
-
-
-
-
-
 void rcmonitorstart() {
     bluetoothStart();
 }
 
 boolean rcmonitor() {
     // Monitor change in Bluetooth connection status
-#ifdef ESP32
+
     boolean isConnected = pServer->getConnectedCount();
-#else
-    boolean isConnected = Bluefruit.connected();
-#endif
+
     if (wasConnected != isConnected) {
         wasConnected = isConnected;
 
@@ -379,6 +252,3 @@ boolean rcmonitor() {
     }
 	return 0;
 }
-
-
-// end 
